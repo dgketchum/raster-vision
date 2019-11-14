ae8c842efdf3aa4e59681b2908f038ee0d29e85b
dgketchum <dgketchum@gmail.com>
2019-11-13 20:59:20 -0700

    stuck hacky box and mask stack in __getitem__ for now

--- rastervision/backend/torch_utils/instance_segmentation/data.py
+++ rastervision/backend/torch_utils/instance_segmentation/data.py
@@ -2,8 +2,10 @@ from os.path import join, basename
 import glob
 
 from PIL import Image
+import numpy as np
 import torchvision
 from torch.utils.data import DataLoader, Dataset
+from torch import as_tensor, tensor, float32, int64, uint8, ones, zeros
 
 from rastervision.backend.torch_utils.data import DataBunch
 
@@ -26,7 +28,7 @@ class ComposeTransforms(object):
         return x, y
 
 
-class SegmentationDataset(Dataset):
+class InstanceSegmentationDataset(Dataset):
     def __init__(self, data_dir, transforms=None):
         self.data_dir = data_dir
         self.img_paths = glob.glob(join(data_dir, 'img', '*.png'))
@@ -37,10 +39,33 @@ class SegmentationDataset(Dataset):
         label_path = join(self.data_dir, 'labels', basename(img_path))
         x = Image.open(img_path)
         y = Image.open(label_path)
+        mask = np.array(y)
+        features = np.unique(mask)
+        features = features[1:]
+        masks = mask == features[:, None, None]
+        boxes = []
+        for i in range(len(features)):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+        boxes = as_tensor(boxes, dtype=float32)
+        # following assumes one class
+        labels = ones(tuple(features), dtype=int64)
+        masks = as_tensor(masks, dtype=uint8)
+        image_id = tensor([ind])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        iscrowd = zeros(tuple(features), dtype=int64)
 
         if self.transforms is not None:
             x, y = self.transforms(x, y)
-        return (x, y)
+
+        target = {'boxes': boxes, 'labels': labels, 'masks': masks, 'label_arr': y,
+                  'image_id': image_id, 'area': area, 'iscrowd': iscrowd}
+
+        return x, target
 
     def __len__(self):
         return len(self.img_paths)
@@ -57,8 +82,8 @@ def build_databunch(data_dir, img_sz, batch_sz, class_names):
     aug_transforms = ComposeTransforms([ToTensor()])
     transforms = ComposeTransforms([ToTensor()])
 
-    train_ds = SegmentationDataset(train_dir, transforms=aug_transforms)
-    valid_ds = SegmentationDataset(valid_dir, transforms=transforms)
+    train_ds = InstanceSegmentationDataset(train_dir, transforms=aug_transforms)
+    valid_ds = InstanceSegmentationDataset(valid_dir, transforms=transforms)
 
     train_dl = DataLoader(
         train_ds,
