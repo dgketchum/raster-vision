9f6242e62591ab0fdbda56724f746f298de10849
dgketchum <dgketchum@gmail.com>
2019-11-17 17:26:44 -0700

    add intersection over union

--- rastervision/backend/torch_utils/metrics.py
+++ rastervision/backend/torch_utils/metrics.py
@@ -28,3 +28,27 @@ def compute_conf_mat_metrics(conf_mat, eps=1e-6):
         'f1': weighted_f1.item()
     }
     return metrics
+
+
+def intersection_over_union_bbox(a_box, b_box):
+
+    if a_box.shape[-1] == 2 or len(a_box.shape) == 1:
+        a_box = a_box.reshape(1, 4) if len(a_box.shape) <= 2 else a_box.reshape(a_box.shape[0], 4)
+    if b_box.shape[-1] == 2 or len(b_box.shape) == 1:
+        b_box = b_box.reshape(1, 4) if len(b_box.shape) <= 2 else b_box.reshape(b_box.shape[0], 4)
+    point_num = max(a_box.shape[0], b_box.shape[0])
+    b1p1, b1p2, b2p1, b2p2 = a_box[:, :2], a_box[:, 2:], b_box[:, :2], b_box[:, 2:]
+
+    # mask that eliminates non-intersecting matrices
+    base_mat = torch.ones((point_num,))
+    base_mat *= torch.all(torch.gt(b1p2 - b2p1, 0), 1)
+    base_mat *= torch.all(torch.gt(b2p2 - b1p1, 0), 1)
+
+    # I area
+    intersect_area = torch.prod(torch.min(b2p2, b1p2) - torch.max(b1p1, b2p1), 1)
+    # U area
+    union_area = torch.prod(b1p2 - b1p1, 1) + torch.prod(b2p2 - b2p1, 1) - intersect_area
+    # IoU
+    intersect_ratio = intersect_area / union_area
+
+    return base_mat * intersect_ratio
