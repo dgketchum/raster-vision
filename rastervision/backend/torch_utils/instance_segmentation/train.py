953c04521252a645352406c8217a4c1a9a0123e6
dgketchum <dgketchum@gmail.com>
2019-11-18 00:50:32 -0700

    return loss as dict of floats

--- rastervision/backend/torch_utils/instance_segmentation/train.py
+++ rastervision/backend/torch_utils/instance_segmentation/train.py
@@ -18,11 +18,12 @@ def train_epoch(model, device, data_loader, opt, loss_fn, step_scheduler=None):
             opt.zero_grad()
 
             loss = out['loss_classifier'] + out['loss_box_reg'] + out['loss_mask'] + \
-                   out['loss_objectness'] + 0.1 * out['loss_rpn_box_reg']
+                   out['loss_objectness'] + 0.0625 * out['loss_rpn_box_reg']
+
+            loss.backward()
 
             total_loss += torch.tensor([v for (k, v) in out.items()]).sum()
 
-            loss.backward()
             opt.step()
 
         if step_scheduler:
@@ -30,7 +31,10 @@ def train_epoch(model, device, data_loader, opt, loss_fn, step_scheduler=None):
 
         num_samples += len(x)
 
-    return out
+        loss_floats = {k: v.item() for (k, v) in out.items()}
+        loss_floats['total_loss'] = total_loss
+
+    return loss_floats
 
 
 def validate_epoch(model, device, data_loader, num_labels):
@@ -39,13 +43,9 @@ def validate_epoch(model, device, data_loader, num_labels):
     conf_mat = torch.zeros((num_labels, num_labels))
     with torch.no_grad():
         with click.progressbar(data_loader, label='Validating') as bar:
-            for batch_ind, (x, y) in enumerate(bar):
-                x = x.to(device)
-                out = model(x)['out']
-
-                out = out.argmax(1).view(-1).cpu()
-                y = y.view(-1).cpu()
-                conf_mat += compute_conf_mat(out, y, num_labels)
+            for batch_ind, (x, target) in enumerate(bar):
+                x = [_.to(device) for _ in x]
+                out = model(x)
 
     # Ignore index zero.
     conf_mat = conf_mat[1:, 1:]
