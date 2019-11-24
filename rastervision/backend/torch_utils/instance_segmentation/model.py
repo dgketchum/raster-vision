09bb70ea8741a564a1cebee1ef53347f1bf0adb3
dgketchum <dgketchum@gmail.com>
2019-11-23 17:37:57 -0700

    option to train from scratch

--- rastervision/backend/torch_utils/instance_segmentation/model.py
+++ rastervision/backend/torch_utils/instance_segmentation/model.py
@@ -1,4 +1,4 @@
-from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.models.detection import maskrcnn_resnet50_fpn
 from torchvision.models.detection.mask_rcnn import MaskRCNN
 from torchvision.models.utils import load_state_dict_from_url
 
@@ -8,19 +8,24 @@ model_urls = {
 }
 
 
-def get_model(model_arch, num_labels, pretrained=False, progress=True):
+def get_model(num_classes, pretrained=True):
 
     if pretrained:
-        pretrained_backbone = False
-    else:
-        pretrained_backbone = True
-
-    backbone = resnet_fpn_backbone(model_arch, pretrained_backbone)
-    model = MaskRCNN(backbone, num_labels)
-    if pretrained:
-        state_dict = load_state_dict_from_url(model_urls['maskrcnn_resnet50_fpn_coco'],
-                                              progress=progress)
-        # noinspection PyUnresolvedReferences
+        assert num_classes == 91
+        model = maskrcnn_resnet50_fpn(pretrained=True)
+        backbone = model.backbone
+        model = MaskRCNN(backbone, num_classes,
+                         image_mean=(0.485, 0.456, 0.406), image_std=(0.229, 0.224, 0.225),
+                         rpn_pre_nms_top_n_test=15, rpn_nms_thresh=0.5, box_score_thresh=0.5,
+                         box_nms_thresh=0.5)
+        state_dict = load_state_dict_from_url(model_urls['maskrcnn_resnet50_fpn_coco'])
         model.load_state_dict(state_dict)
+
+    else:
+        backbone = maskrcnn_resnet50_fpn(pretrained=False).backbone
+        model = MaskRCNN(backbone, num_classes,
+                         image_mean=(0.485, 0.456, 0.406), image_std=(0.229, 0.224, 0.225),
+                         rpn_pre_nms_top_n_test=15, rpn_nms_thresh=0.5, box_score_thresh=0.5,
+                         box_nms_thresh=0.5)
 
     return model
