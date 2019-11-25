f39ce29a5db23963342cc935c0fe1a3618e42a0a
dgketchum <dgketchum@gmail.com>
2019-11-24 19:52:30 -0700

    trains an epoch without going to nan or inf

--- rastervision/backend/torch_utils/instance_segmentation/train.py
+++ rastervision/backend/torch_utils/instance_segmentation/train.py
@@ -7,35 +7,32 @@ from rastervision.backend.torch_utils.metrics import (compute_conf_mat,
 
 def train_epoch(model, device, data_loader, opt, loss_fn, step_scheduler=None):
     model.train()
-    total_loss = 0
     num_samples = 0
 
     with click.progressbar(data_loader, label='Training') as bar:
         for batch_ind, (x, target) in enumerate(bar):
+            x = [t.to(device) for t in x]
+            paths = [x['path'] for x in target]
+            target = [{k: v.to(device) for (k, v) in dict_.items() if 'path' not in k} for dict_ in target]
 
-            # check for empty boxes, which corrupt the model
-            # should this be in data.py?
-            not_empty = [True if t['boxes'].size()[0] > 0 else False for t in target]
-            target = [{k: v.to(device) for (k, v) in dict_.items()} for dict_, c in zip(target, not_empty) if c]
+            if len(target) > 0:
+                out = model(x, target)
+                opt.zero_grad()
 
-            x = [t.to(device) for t, c in zip(x, not_empty) if c]
+                loss = out['loss_classifier'] + out['loss_box_reg'] + out['loss_mask'] + \
+                       out['loss_objectness'] + out['loss_rpn_box_reg']
 
-            out = model(x, target)
-            opt.zero_grad()
+                loss.backward()
+                opt.step()
+                if step_scheduler:
+                    step_scheduler.step()
 
-            loss = out['loss_classifier'] + out['loss_box_reg'] + out['loss_mask'] + \
-                   out['loss_objectness'] + out['loss_rpn_box_reg']
-
-            loss.backward()
-            opt.step()
-            if step_scheduler:
-                step_scheduler.step()
-
-            loss_floats = {k: v.item() for (k, v) in out.items()}
-            if torch.isnan(loss):
                 num_samples += len(x)
+                loss_floats = {k: v.item() for (k, v) in out.items()}
 
-            print(len(x))
+                if torch.isnan(loss):
+                    print(num_samples, paths)
+
         return loss_floats
 
 
