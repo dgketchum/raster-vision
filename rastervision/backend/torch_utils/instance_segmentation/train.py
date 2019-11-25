23906d07f40f6f262995eff31523389a7d6ec763
dgketchum <dgketchum@gmail.com>
2019-11-24 18:53:52 -0700

    chek for empty boxes in train_epoch

--- rastervision/backend/torch_utils/instance_segmentation/train.py
+++ rastervision/backend/torch_utils/instance_segmentation/train.py
@@ -1,6 +1,6 @@
 import click
 import torch
-
+from torch.autograd import detect_anomaly
 from rastervision.backend.torch_utils.metrics import (compute_conf_mat,
                                                       compute_conf_mat_metrics)
 
@@ -12,31 +12,31 @@ def train_epoch(model, device, data_loader, opt, loss_fn, step_scheduler=None):
 
     with click.progressbar(data_loader, label='Training') as bar:
         for batch_ind, (x, target) in enumerate(bar):
-            x = [_.to(device) for _ in x]
-            target = [{k: v.to(device) for (k, v) in dict_.items()} for dict_ in target]
+
+            # check for empty boxes, which corrupt the model
+            # should this be in data.py?
+            not_empty = [True if t['boxes'].size()[0] > 0 else False for t in target]
+            target = [{k: v.to(device) for (k, v) in dict_.items()} for dict_, c in zip(target, not_empty) if c]
+
+            x = [t.to(device) for t, c in zip(x, not_empty) if c]
+
             out = model(x, target)
             opt.zero_grad()
 
-            # TODO figure out why loss_rpn_box_reg gos to inf
-            # removed 'loss_rpn_box_reg' because it goes to inf
             loss = out['loss_classifier'] + out['loss_box_reg'] + out['loss_mask'] + \
-                   out['loss_objectness']
+                   out['loss_objectness'] + out['loss_rpn_box_reg']
 
             loss.backward()
-
-            total_loss += torch.tensor([v for (k, v) in out.items()]).sum()
-
             opt.step()
+            if step_scheduler:
+                step_scheduler.step()
 
-        if step_scheduler:
-            step_scheduler.step()
+            loss_floats = {k: v.item() for (k, v) in out.items()}
+            if torch.isnan(loss):
+                num_samples += len(x)
 
-        num_samples += len(x)
-
-        loss_floats = {k: v.item() for (k, v) in out.items()}
-        loss_floats['total_loss'] = total_loss
-
-    return loss_floats
+            print(len(x))
+        return loss_floats
 
 
 def validate_epoch(model, device, data_loader, num_labels):
@@ -46,8 +46,9 @@ def validate_epoch(model, device, data_loader, num_labels):
     with torch.no_grad():
         with click.progressbar(data_loader, label='Validating') as bar:
             for batch_ind, (x, target) in enumerate(bar):
-                x = [_.to(device) for _ in x]
-                out = model(x)
+                pass
+                # x = [_.to(device) for _ in x]
+                # out = model(x)
 
     # Ignore index zero.
     conf_mat = conf_mat[1:, 1:]
