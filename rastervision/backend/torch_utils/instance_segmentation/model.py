fc7cd43222077d51add801550b523ffad017adb8
dgketchum <dgketchum@gmail.com>
2019-11-27 09:15:08 -0700

    freeze head/ this disagrees with approach taken in object detection

--- rastervision/backend/torch_utils/instance_segmentation/model.py
+++ rastervision/backend/torch_utils/instance_segmentation/model.py
@@ -2,18 +2,26 @@ from torchvision.models.detection import maskrcnn_resnet50_fpn
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
-def get_model(num_classes=91, pretrained=True):
+
+def get_model(num_classes=91, pretrained=False):
 
     if pretrained or num_classes == 91:
         assert num_classes == 91
         model = maskrcnn_resnet50_fpn(pretrained=True)
 
     elif num_classes != 91:
+
         # TODO: specify user options for hyper-parameters
         model = maskrcnn_resnet50_fpn(pretrained=True)
+
+        for name, parameter in model.named_parameters():
+            if 'rpn.head' not in name and 'roi_heads' not in name:
+                parameter.requires_grad_(False)
+
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
         in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+
         hidden_layer = 256
         model.roi_heads.mask_predictor = MaskRCNNPredictor(in_channels=in_features_mask,
                                                            dim_reduced=hidden_layer,
